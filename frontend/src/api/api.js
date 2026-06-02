8e8080d2ff05052eb0d5925237c3391ea59f5364
youssefel01 <elyarchouhiy@gmail.com>
2026-06-02 23:08:55 +0100

    fix prod dmain on build

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8010';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL !== undefined ? import.meta.env.VITE_API_BASE_URL : '';
 
 const client = axios.create({
   baseURL: BASE_URL,
